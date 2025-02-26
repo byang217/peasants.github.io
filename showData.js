d0e7520cf9b40494362be7d3b98032bbf24e8aa0
Enderbluedog06 <159450783+Enderbluedog06@users.noreply.github.com>
2025-02-26 13:43:01 -0500

    Works now

--- showData.js
+++ showData.js
@@ -1,17 +1,15 @@
-// submbtn = document.getElementById('submit');
-// submbtn.addEventListener('click', queryCheckboxes);
-/*
-console.log(sheetData);
-document.addEventListener('DOMContentLoaded', function() {
-    function queryCheckboxes() {
-        console.log("Running");
+submbtn = document.getElementById('submit');
+submbtn.addEventListener('click', queryCheckboxes);
+
+function queryCheckboxes() 
+{
         let total = 0;
         let checkedBox = [];
         // Ensures that only checked values are printed
         for (let i = 1; i < sheetData.length + 1; i++) {
             let checkbox = document.getElementById('prgm' + i);
             if (checkbox && checkbox.checked) {
-                checkedBox.push(i - 1);
+                checkedBox.push(i);
                 total++;
             }
         }
@@ -19,21 +17,50 @@ document.addEventListener('DOMContentLoaded', function() {
         if (total > 5) {
             alert("You can only compare 5 programs at a time");
         }
+        else {
         showData(total, checkedBox);
-    }
-
-    // Call the function to test if it's running
-    queryCheckboxes();
-}); 
+        }
+}
 function showData(total, checkedBox)
 {
-    console.log(checkedBox[1]);
-    for (let i = 1; i < checkedBox.length; i++)
+    table = document.getElementById('prgmTable');
+    tableString = ""
+    if (table.innerHTML != "")
     {
-        table = document.getElementById('prgmTable');
-        addRecord = `<tr><th>${sheetData[checkedBox[i]][1]}</th></tr>`;
-        table.innerHTML = addRecord;
+        table.innerHTML = "";
     }
-   
+    
+    for (let i = 0; i < checkedBox.length; i++)
+    {
+        if (i == 0)
+        {
+            tableString += `<tr><th>${sheetData[0][1]}</th>`
+        }
+
+        addHeader = `<th>${sheetData[checkedBox[i]][1]}</th>`;
+        tableString += addHeader;
+        if (checkedBox.length == i)
+        {
+            tableString += "</tr>"
+        }
+    }
+    
+    for (let i = 2; i < sheetData[0].length; i++)
+    {
+        tableString += "<tr>"
+        for (let ii = 0; ii < checkedBox.length; ii++)
+            {
+                if (ii == 0)
+                {
+                    tableString += `<td>${sheetData[0][i]}</td>`;
+                }
+                addRecord = `<td>${sheetData[checkedBox[ii]][i]}</td>`
+                tableString += addRecord;
+                console.log("I'm iterating for the: " + ii + "th time!");
+            }
+    }
+    
+    
+    console.log(tableString);
+    table.innerHTML += tableString;
 }
-    */ 
