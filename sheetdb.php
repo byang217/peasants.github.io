24e9015d4f1a703e73aed59b7febcad26fe2edb3
Enderbluedog06 <159450783+Enderbluedog06@users.noreply.github.com>
2025-04-02 10:43:51 -0400

    Sheetdb.php includes error handling for table data

--- sheetdb.php
+++ sheetdb.php
@@ -1,79 +1,8 @@
 <?php
-/*
 
-function getDataFromSheet($apiId, $parameters = [])
-{
-    $url = "https://sheetdb.io/api/v1/" . $apiId;
-
-    if(!empty($parameters))
-    {
-        $url .= '?' . http_build_query($parameters);
-    }
-
-    $response = file_get_contents($url);
-    return json_decode($response, true);
-}
-
-$sheetData = getDataFromSheet('tqqbkwd9znd7s', []);
-
-#print_r($sheetData);
-#echo $sheetData[0]['PROGRAM_NAME'];
-
-foreach($sheetData as $recordnum => $recorddata)
-{
-    foreach($recorddata as $field => $record)
-    {
-        // Remove $field from the echo if you need to
-        // echo "$field: $record <br>";
-    }
-   
-}
-
-*/
-
-
-/*
-require_once 'config.php';
-
-    $spreadsheetId = '1-rMk_8c_HGF3n4cKTPlyNs_lFWbCOgduZysMJufFuSg';
-
-    $client = new Google_Client();
-
-    $db = new DB();
-
-    $arr_token = (array) $db->get_access_token();
-    $accessToken = array(
-        'access_token' => $arr_token['access_token'],
-        'expires_in' => $arr_token['expires_in'],
-    );
-
-    $client->setAccessToken($accessToken);
-
-    $service = new Google_Service_Sheets($client);
-
-    $range = 'Programs';
-    $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-    $sheetData = $response->getValues();
-    print_r($sheetData);
-
-    json_encode($sheetData);
-    
-    echo "<br><br><br><br><br>";
-
-    print_r($sheetData);
-
-
-    #echo $sheetData[0][0];
-*/
-
-
-/*
-This code REQUIRES you to run the following command in your VS Code terminal: composer require google/apiclient:^2.0
-
-You WILL need to install composer if you don't already have it, you can do so at this link: https://getcomposer.org/
-*/
 
 require 'vendor/autoload.php';
+
 
 $client = new \Google_Client();
 $client->setApplicationName('Google Sheets API PHP');
@@ -84,10 +13,32 @@ $service = new \Google_Service_Sheets($client);
 $spreadsheetId = '1-rMk_8c_HGF3n4cKTPlyNs_lFWbCOgduZysMJufFuSg';
 $range = 'Programs'; 
 $response = $service->spreadsheets_values->get($spreadsheetId, $range);
-$sheetData = $response->getValues();
+$fullSheetData = $response->getValues();
 
-#print_r($sheetData);
+$sheetData = [];
 
+foreach ($fullSheetData as $row)
+{
+    // Ensures that empty rows in between data don't break things.
+    if (empty($row))
+    {
+        continue;
+    }
+    // Ensures that only rows with 'Yes' in the 'Publish to Web' column are delivered.
+    if ((isset($row[0]) && strtolower($row[0]) === 'yes') || ($row[0] === 'Publish to Web'))
+    {
+        $sheetData[] = $row;
+    }
+}
+// Changes Program ID Values if a non-ending row is not published (ex. If Row 4 with Program ID 3 isn't published, decrease all program ids by one for all programs after row 4).
+for ($i = 1; $i < count($sheetData); $i++)
+{
+    if ((int)$sheetData[$i][1] !== $i)
+    {
+
+        (int)$sheetData[$i][1] = $i;
+    }
+}
 
 
 ?>
